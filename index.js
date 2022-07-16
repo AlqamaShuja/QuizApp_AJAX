465754f680270ddf9868c4513f2f21ff57cb5025
AlqamaShuja <alqamashuja313@gmail.com>
2022-07-16 13:14:17 +0500

    done with start again button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var quizData = [];
 var i = 0, score = 0;
+var userAllSelectedValue = [];
 
 $(document).ready(function () {
     $.ajax({
@@ -11,35 +12,72 @@ $(document).ready(function () {
         }
     });
 
-    $("#nextBtn").on("click", function () {
+    $(document).on("click", "#nextBtn", function () {
         selectedValue = $('input[name="quiz"]:checked').val();
-        if (selectedValue === quizData[i].correctAnswer) {
-            score++;
-        }
+        userAllSelectedValue[i] = selectedValue;
+        
         i++;
         if (i !== quizData.length) {
             getQuestion();
+            disabledBtn(i);
 
         } else {
+            for(let k=0; k<quizData.length; k++){
+                if (userAllSelectedValue[k] === quizData[k].correctAnswer) {
+                    score++;
+                }
+            }
             $(".container").html(`
                 <h1>Quiz End</h1>
-                <h3>Your Score is ${score}</h3>
+                <h3>Your Score is ${score} out of ${quizData.length}</h3>
                 <button id="startAgainBtn">Start Again</button>
             `);
         }
     });
 
-    $("#startAgainBtn").click(function () {
-        console.log("Click Again start")
+    disabledBtn(i);
+    
+
+    $(document).on("click", "#previousBtn", function () {
+        i--;
+        getQuestion();
+        $(`input[name=quiz][id=${userAllSelectedValue[i]}]`).prop("checked",true);
+        console.log(userAllSelectedValue);
+    });
+
+    $(document).on("click", "#startAgainBtn", function () {
         i = 0;
+        score = 0;
+        userAllSelectedValue = [];
         getQuestion();
     })
 
 });
 
 
+function disabledBtn(i){
+    if (i === 0) {
+        $("#previousBtn").prop('disabled', true);
+    }else{
+        $("#previousBtn").prop('disabled', false);
+    }
+}
+
+
 function getQuestion() {
-    data = quizData[i]
+    $(".container").html(`<h1 class="quiz-head">Quiz</h1>
+                        <div class="quiz-content">
+                            <div class="quiz-question">Question Apear here</div>
+                            <div class="quiz-answer">
+                            </div>
+                            <div id="btn-div">
+                                <button id="previousBtn" disabled>Previous</button>
+                                <button id="nextBtn">Next</button>
+                            </div>
+                        </div>`
+    );
+
+    data = quizData[i];
     $(".quiz-question").html(data.question);
     // let inp = document.createElement("input")
     var output = ""
